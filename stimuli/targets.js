9976aa1d557698190faac600d19de7173969b672
Angus-Ball <balla2@myumanitoba.ca>
2024-03-20 13:24:05 -0500

    Initial commit

---
+++ stimuli/targets.js
@@ -0,0 +1,15 @@
+// Study List Words
+var targets = [
+     {target: "abdicate",     data: {"name":"abdicate"}},
+     {target: "allocate",     data: {"name":"allocate"}},
+     {target: "asbestos",     data: {"name":"asbestos"}},
+     {target: "decanter",     data: {"name":"decanter"}},
+     {target: "deplete",      data: {"name":"deplete"}},
+     {target: "incubate",     data: {"name":"incubate"}},
+     {target: "instigate",    data: {"name":"instigate"}},
+     {target: "javelin",      data: {"name":"javelin"}},
+     {target: "linen",        data: {"name":"linen"}},
+     {target: "sextant",      data: {"name":"sextant"}},
+     {target: "tariff",       data: {"name":"tariff"}},
+     {target: "tranquilizer", data: {"name":"tranquilizer"}}  
+];
