6634c9e620ca57773592c2467439bfcb8ee3643f
Angus-Ball <balla2@myumanitoba.ca>
2024-04-04 00:40:37 -0500

    Update targets.js

--- stimuli/targets.js
+++ stimuli/targets.js
@@ -1,33 +1,33 @@
 // Study List Words
 var targets = [
-     {target: "abdicate",     data: {"phase":"study"}},
-     {target: "allocate",     data: {"phase":"study"}},
-     {target: "asbestos",     data: {"phase":"study"}},
-     {target: "decanter",     data: {"phase":"study"}},
-     {target: "deplete",      data: {"phase":"study"}},
-     {target: "incubate",     data: {"phase":"study"}},
-     {target: "instigate",    data: {"phase":"study"}},
-     {target: "javelin",      data: {"phase":"study"}},
-     {target: "linen",        data: {"phase":"study"}},
-     {target: "sextant",      data: {"phase":"study"}},
-     {target: "tariff",       data: {"phase":"study"}},
-     {target: "tranquilizer", data: {"phase":"study"}}  
+     {target: "<font size = '+3'>abdicate</font>",     data: {"phase":"study"}},
+     {target: "<font size = '+3'>allocate</font>",     data: {"phase":"study"}},
+     {target: "<font size = '+3'>asbestos</font>",     data: {"phase":"study"}},
+     {target: "<font size = '+3'>decanter</font>",     data: {"phase":"study"}},
+     {target: "<font size = '+3'>deplete</font>",      data: {"phase":"study"}},
+     {target: "<font size = '+3'>incubate</font>",     data: {"phase":"study"}},
+     {target: "<font size = '+3'>instigate</font>",    data: {"phase":"study"}},
+     {target: "<font size = '+3'>javelin</font>",      data: {"phase":"study"}},
+     {target: "<font size = '+3'>linen</font>",        data: {"phase":"study"}},
+     {target: "<font size = '+3'>sextant</font>",      data: {"phase":"study"}},
+     {target: "<font size = '+3'>tariff</font>",       data: {"phase":"study"}},
+     {target: "<font size = '+3'>tranquilizer</font>", data: {"phase":"study"}}  
 ];
 
 var block_1 = [
-     {target: "<font size = '+2'>allocate</font>",   data: {"phase":"study_1"}},
-     {target: "decanter",   data: {"phase":"study_1"}},
-     {target: "<font size = '+2'>deplete</font>",   data: {"phase":"study_1"}},
-     {target: "linen",   data: {"phase":"study_1"}},
-     {target: "<font size = '+2'>sextant</font>",   data: {"phase":"study_1"}},
-     {target: "tranquilizer",   data: {"phase":"study_1"}}
+     {target: "<font size = '+3'>allocate</font>",   data: {"phase":"study_1"}},
+     {target: "<font size = '+3'>decanter</font>",   data: {"phase":"study_1"}},
+     {target: "<font size = '+3'>deplete</font>",   data: {"phase":"study_1"}},
+     {target: "<font size = '+3'>linen</font>",   data: {"phase":"study_1"}},
+     {target: "<font size = '+3'>sextant</font>",   data: {"phase":"study_1"}},
+     {target: "<font size = '+3'>tranquilizer</font>",   data: {"phase":"study_1"}}
 ];
 
 var block_2 = [
-     {target: "abdicate",   data: {"phase":"study_2"}},
-     {target: "asbestos",   data: {"phase":"study_2"}},
-     {target: "incubate",   data: {"phase":"study_2"}},
-     {target: "instigate",   data: {"phase":"study_2"}},
-     {target: "javelin",   data: {"phase":"study_2"}},
-     {target: "tariff",   data: {"phase":"study_2"}}
+     {target: "<font size = '+3'>abdicate</font>",   data: {"phase":"study_2"}},
+     {target: "<font size = '+3'>asbestos</font>",   data: {"phase":"study_2"}},
+     {target: "<font size = '+3'>incubate</font>",   data: {"phase":"study_2"}},
+     {target: "<font size = '+3'>instigate</font>",   data: {"phase":"study_2"}},
+     {target: "<font size = '+3'>javelin</font>",   data: {"phase":"study_2"}},
+     {target: "<font size = '+3'>tariff</font>",   data: {"phase":"study_2"}}
 ];
