9c0083c3bd4f55e598f8752ac6ac6fe85920a3cb
Angus-Ball <balla2@myumanitoba.ca>
2024-04-09 12:48:08 -0500

    Update targets.js

--- stimuli/targets.js
+++ stimuli/targets.js
@@ -15,19 +15,19 @@ var targets = [
 ];
 
 var block_1 = [
-     {target: "<font size = '+3'>allocate</font>",     data: {"phase":"study_1"}},
-     {target: "<font size = '+3'>decanter</font>",     data: {"phase":"study_1"}},
-     {target: "<font size = '+3'>deplete</font>",      data: {"phase":"study_1"}},
-     {target: "<font size = '+3'>linen</font>",        data: {"phase":"study_1"}},
-     {target: "<font size = '+3'>sextant</font>",      data: {"phase":"study_1"}},
-     {target: "<font size = '+3'>tranquilizer</font>", data: {"phase":"study_1"}}
+     {target: "<font size='+3'>allocate</font>",     data: {"phase":"study_1"}},
+     {target: "<font size='+3'>decanter</font>",     data: {"phase":"study_1"}},
+     {target: "<font size='+3'>deplete</font>",      data: {"phase":"study_1"}},
+     {target: "<font size='+3'>linen</font>",        data: {"phase":"study_1"}},
+     {target: "<font size='+3'>sextant</font>",      data: {"phase":"study_1"}},
+     {target: "<font size='+3'>tranquilizer</font>", data: {"phase":"study_1"}}
 ];
 
 var block_2 = [
-     {target: "<font size = '+3'>abdicate</font>",     data: {"phase":"study_2"}},
-     {target: "<font size = '+3'>asbestos</font>",     data: {"phase":"study_2"}},
-     {target: "<font size = '+3'>incubate</font>",     data: {"phase":"study_2"}},
-     {target: "<font size = '+3'>instigate</font>",    data: {"phase":"study_2"}},
-     {target: "<font size = '+3'>javelin</font>",      data: {"phase":"study_2"}},
-     {target: "<font size = '+3'>tariff</font>",       data: {"phase":"study_2"}}
+     {target: "<font size='+3'>abdicate</font>",     data: {"phase":"study_2"}},
+     {target: "<font size='+3'>asbestos</font>",     data: {"phase":"study_2"}},
+     {target: "<font size='+3'>incubate</font>",     data: {"phase":"study_2"}},
+     {target: "<font size='+3'>instigate</font>",    data: {"phase":"study_2"}},
+     {target: "<font size='+3'>javelin</font>",      data: {"phase":"study_2"}},
+     {target: "<font size='+3'>tariff</font>",       data: {"phase":"study_2"}}
 ];
